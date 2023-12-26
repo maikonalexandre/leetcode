fd035cddf05e9702ed930fc0327c8f6a5cc898dd
Maike <maikonalexandre574@gmail.com>
2023-12-26 18:20:48 -0300

    solve: leet code 67

---
+++ 67. Add Binary/index.js
@@ -0,0 +1,22 @@
+var addBinary = function (a, b) {
+  a = a.toString();
+  b = b.toString();
+
+  let len = a.length > b.length ? a.length - 1 : b.length - 1;
+
+  const sum = [];
+  let hand = 0;
+
+  for (len; len >= 0; len--) {
+    
+  }
+
+  //expect 100
+
+  //0 1 1
+  //0 0 1
+  //------
+  //1 0 0
+};
+
+console.log(addBinary(11, 1));
