f3e4db7f575ff22d287aaa11598b729dcd2e1fcb
Maike <maikonalexandre574@gmail.com>
2024-01-16 16:59:09 -0300

    solve: leet code 67

--- 67. Add Binary/index.js
+++ 67. Add Binary/index.js
@@ -1,22 +1,18 @@
 var addBinary = function (a, b) {
-  a = a.toString();
-  b = b.toString();
-
-  let len = a.length > b.length ? a.length - 1 : b.length - 1;
-
-  const sum = [];
   let hand = 0;
+  let str = "";
 
-  for (len; len >= 0; len--) {
-    
+  for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; j--, i--) {
+    let res = ((Number(a[i])) || 0) + ((Number(b[j])) || 0) + hand
+    str = (res % 2) + str
+    hand = res > 1 ? 1 : 0
   }
 
-  //expect 100
-
-  //0 1 1
-  //0 0 1
-  //------
-  //1 0 0
+  return hand > 0 ? hand + str : str
 };
 
-console.log(addBinary(11, 1));
+console.log(addBinary("11", "1") == "100");
+
+var _addBinary = function (a, b) {
+  return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2)
+};
