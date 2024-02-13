5122c5424327725402b58797cdb9c0aacbd515de
Maike <maikonalexandre574@gmail.com>
2024-02-13 14:57:15 -0300

    solve: leet code 70

---
+++ 70. Climbing Stairs/index.js
@@ -0,0 +1,17 @@
+var climbStairs = function (factor) {
+    // 1 - 1
+    // 2 - 2
+    // 3 - 3
+    // 4 - 5
+    // 5 - 8
+    // 6 - 13
+    // 7 - 21 
+
+    for (let index = 1; index <= factor; index++) {
+        console.log("[FACTOR]", index);
+    }
+
+
+};
+
+climbStairs(7)
