fd0a7f39a929ab5af0726e9f22a941e667bc56e5
Maike <maikonalexandre574@gmail.com>
2024-03-22 13:37:45 -0300

    solve: leetcode 28

---
+++ 28. Find the Index of the First Occurrence in a String/index.js
@@ -0,0 +1,28 @@
+var strStr = function (haystack, needle) {
+    function findFrom(i) {
+        if (haystack[i] === undefined) return -1;
+        if (haystack[i] === needle[0]) {
+            const initial = i;
+            i++;
+            let next = -1; // In case the first letter appears again
+            let j = 1;
+            let matches = true;
+            while (needle[j]) {
+                if (haystack[i] === needle[0] && next === -1) next = i;
+                if (needle[j] !== haystack[i]) {
+                    if (next !== -1) return findFrom(next);
+                    matches = false;
+                    break;
+                }
+                i++;
+                j++;
+            }
+
+            if (matches) return initial;
+            return findFrom(i);
+        }
+        return findFrom(++i);
+    }
+
+    return findFrom(0);
+};
