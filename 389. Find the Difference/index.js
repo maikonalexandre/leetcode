3aef298f72264e5380d7b42bea7bda40489d28e4
Maike <maikonalexandre574@gmail.com>
2024-03-25 15:47:57 -0300

    solve: leet code 389

--- 389. Find the Difference/index.js
+++ 389. Find the Difference/index.js
@@ -1,1 +1,31 @@
-var findTheDifference = function (s, t) { };
+var findTheDifference = function (s, t) {
+    const map = new Map()
+
+    for (const i in s) {
+        const key = s[i]
+        if (map.has(key)) {
+            const previousValue = map.get(key)
+            map.set(key, previousValue + 1)
+        } else {
+            map.set(key, 1)
+        }
+    }
+
+
+    for (const i in t) {
+        const key = t[i]
+        if (map.has(key)) {
+            const previousValue = map.get(key)
+
+            if (previousValue === 0) {
+                return key
+            }
+
+            map.set(key, previousValue - 1)
+        } else {
+            return key
+        }
+    }
+};
+
+console.log(findTheDifference("aabcd", "aabecd"))
