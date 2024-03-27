bc2d09eeac2400bacdcbe58e9f0a96abb3ba7677
Maike <maikonalexandre574@gmail.com>
2024-03-27 14:02:14 -0300

    solve: leet code 392

--- 392. Is Subsequence/index.js
+++ 392. Is Subsequence/index.js
@@ -1,6 +1,17 @@
 var isSubsequence = function (s, t) {
+    let sp = 0
+    let tp = 0
 
+    while (tp < t.length && sp < s.length) {
+        if (s[sp] === t[tp]) {
+            tp++
+            sp++
+        } else {
+            tp++
+        }
+    }
 
+    return sp === s.length
 };
 
-console.log(isSubsequence("aaaaaa", "bbaaaa"));
+console.log(isSubsequence("abc", "aaaabc"));
