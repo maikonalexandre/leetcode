5fedddbac0ce8abb7c91d1ee46e18e604107f4a5
Maike <maikonalexandre574@gmail.com>
2024-05-08 16:16:24 -0300

    solve: leetcode 75

--- 75. Sort Colors/index.js
+++ 75. Sort Colors/index.js
@@ -1,27 +1,30 @@
 var numbers = [1, 2, 0]
 
 var sortColors = function (nums) {
-    let zeroPointer = 0;
-    let twoPointer = nums.length - 1;
-    let i = 0;
-    while (i < nums.length) {
-        while (nums[zeroPointer] == 0) {
-            zeroPointer++;
+    let l = 0;
+    let r = nums.length - 1;
+    let p = 0;
+
+    while (p <= r) {
+        if (nums[p] == 0) {
+            let tmp = nums[l]
+            nums[l] = nums[p]
+            nums[p] = tmp
+            p++
+            l++
         }
-        while (nums[twoPointer] == 2) {
-            twoPointer--;
+
+        else if (nums[p] == 2) {
+            let tmp = nums[r]
+            nums[r] = nums[p]
+            nums[p] = tmp
+            r--
         }
-        if (nums[i] == 0 && i > zeroPointer) {
-            let temp = nums[i];
-            nums[i] = nums[zeroPointer];
-            nums[zeroPointer] = temp;
-        } else if (nums[i] == 2 && i < twoPointer) {
-            let temp = nums[i];
-            nums[i] = nums[twoPointer];
-            nums[twoPointer] = temp;
-        } else {
-            i++;
+
+        else {
+            p++
         }
+
     }
 };
 
