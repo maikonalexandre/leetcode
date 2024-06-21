89a922c788c18a887f60705b53708ae02957c5b1
Maike <maikonalexandre574@gmail.com>
2024-06-21 14:37:35 -0300

    solve: leet code 2540

---
+++ 2540. Minimum Common Value/index.js
@@ -0,0 +1,19 @@
+var getCommon = function (nums1, nums2) {
+    let i = 0
+    let j = 0
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] == nums2[j]) {
+            return nums1[i]
+        }
+        else if (nums1[i] < nums2[j]) {
+            i++
+        } else {
+            j++
+        }
+    }
+
+    return -1
+};
+
+console.log(getCommon(nums1 = [1, 2, 3], nums2 = [2, 4]));
