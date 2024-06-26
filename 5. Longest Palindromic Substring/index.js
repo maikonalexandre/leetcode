af129e83b975a7b929613b8f7a022f0d08ab6ef7
Maike <maikonalexandre574@gmail.com>
2024-06-26 11:22:15 -0300

    solve: leet code 5

---
+++ 5. Longest Palindromic Substring/index.js
@@ -0,0 +1,40 @@
+var longestPalindrome = function (s) {
+    let result = ""
+
+    for (const i in s) {
+        const str = expandPalindrome(Number(i), Number(i))
+        const str2 = expandPalindrome(Number(i), Number(i) + 1)
+
+        console.log(str, str2);
+
+        if (str.length > result.length) {
+            result = str
+        }
+
+        if (str2.length > result.length) {
+            result = str2
+        }
+    }
+
+
+    return result
+
+
+
+
+    function expandPalindrome(left, right) {
+        while (left >= 0 && right < s.length) {
+            if (s[left] == s[right]) {
+                left--
+                right++
+            } else {
+                break
+            }
+        }
+        return s.slice(left + 1, right)
+    }
+};
+
+
+
+console.log("RETURN", longestPalindrome(s = "cbbd"));
