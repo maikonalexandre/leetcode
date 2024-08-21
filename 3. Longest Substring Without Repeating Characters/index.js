ae1275cfde1de77a40d559e9456da8cb3f84b94f
Maike <maikonalexandre574@gmail.com>
2024-08-21 15:14:14 -0300

    solve: add another solution

--- 3. Longest Substring Without Repeating Characters/index.js
+++ 3. Longest Substring Without Repeating Characters/index.js
@@ -30,4 +30,33 @@ var lengthOfLongestSubstring = (s) => {
 // abcabcbb -> 3
 // pwwkew -> 3
 
+// var lengthOfLongestSubstring = function (s) {
+
+//     let longestStr = 0;
+//     let set = new Set();
+
+//     let left = 0;
+//     let right = 0;
+
+//     while (right < s.length) {
+
+//         let letter = s[right];
+
+//         if (!set.has(letter)) {
+//             set.add(letter);
+//             longestStr = Math.max(longestStr, set.size);
+//             right++;
+//         } else {
+//             set.delete(s[left]);
+//             left++;
+//         }
+
+
+//     }
+//     return longestStr;
+
+// };
+
+
+
 console.log(lengthOfLongestSubstring("tmmzuxt"))
